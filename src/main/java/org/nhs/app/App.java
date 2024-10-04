d25cb35d85d6fe11865811b6e83b259aaf4f19fd
Simon <1118080@apps.nsd.org>
2024-10-04 18:59:51 +0000

    Done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,84 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App
+ {
+  public static void main(String args[])
+  {
+  // mystery3();
 
-package org.nhs.app;
+  //  nested();
 
-// Your code goes here
+    practice1();
+
+    System.out.println();
+
+    practice2();
+
+    System.out.println();
+
+    practice3();
+ }
+ 
+ public static void practice1() {
+    for (int num = 1; num < 6; num++) {
+        for (int counter = 0; counter < num; counter++) {
+            System.out.print(num);
+        }
+    System.out.println();
+    }
+ }
+
+ public static void practice2() {
+    char dot = '.';
+    for (int num = 1; num < 6; num++) {
+        for (int i = 5; i > num; i--) {
+            System.out.print(dot);
+            }
+        for (int counter = 0; counter < num; counter++) {
+            System.out.print(num);
+        }
+        System.out.println();
+    }
+    
+    }
+
+
+
+
+ public static void practice3() {
+    char dot = '.';
+    for (int num = 1; num < 6; num++) {
+        for (int i = 5; i > num; i--) {
+            System.out.print(dot);
+            }
+        System.out.print(num);
+        for (int j = 1; j < num; j++) {
+            System.out.print(dot);
+        }
+        System.out.println();
+        }
+    
+    }
+ 
+    public static void challenge() {
+        
+    }
+
+
+ 
+ /* 
+ public static void mystery3() {
+   int foo = 3;
+   for (int number = 0; foo > number; number += 2) {
+    foo++;
+    System.out.println(foo + " " + number + " ");
+} }
+
+   public static void nested() {
+    for (int number = 1; number < 6; number++) {
+        for (int counter = 1; counter < 5; counter++) {
+        System.out.print(number);
+        }
+    System.out.println();
+    }
+   }*/
+
+ }
